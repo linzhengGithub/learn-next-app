e938e7f87fb64a953b1a4258310d3e56a1ba458a
linzheng <alidlg9045@126.com>
2024-03-18 10:58:11 +0800

    数据请求记忆&关闭请求记忆

--- next-data/app/page.js
+++ next-data/app/page.js
@@ -14,17 +14,56 @@
 //   return <img src={data[0].url} width="300" />
 // }
 
-async function getData() {
-  const res = await fetch('https://api.thecatapi.com/v1/images/search', { next: { tags: ['collection'] } })
-  if (!res.ok) {
-    throw new Error('Failed to fetch data')
-  }
+// async function getData() {
+//   const res = await fetch('https://api.thecatapi.com/v1/images/search', { next: { tags: ['collection'] } })
+//   if (!res.ok) {
+//     throw new Error('Failed to fetch data')
+//   }
  
+//   return res.json()
+// }
+
+// export default async function Page() {
+//   const data = await getData()
+  
+//   return <img src={data[0].url} width="300" />
+// }
+
+
+// 强制 fetch 不缓存
+export const fetchCache = 'force-no-store'
+
+async function getData() {
+  // 关闭请求记忆
+  const { signal } = new AbortController()
+  const res = await fetch('https://api.thecatapi.com/v1/images/search', { signal }) 
   return res.json()
+}
+
+export async function generateMetadata() {
+  const data = await getData()
+  return {
+    title: data[0].id
+  }
 }
 
 export default async function Page() {
   const data = await getData()
-  
-  return <img src={data[0].url} width="300" />
+  return (
+    <>
+      <h1>图片 ID：{data[0].id}</h1>
+      <img src={data[0].url} width="300" />
+      <CatDetail />
+    </>
+  )
+}
+
+async function CatDetail() {
+  const data = await getData()
+  return (
+    <>
+      <h1>图片 ID：{data[0].id}</h1>
+      <img src={data[0].url} width="300" />
+    </>
+  )
 }
